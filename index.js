1251de2ddf9493ec56bd69216d1e39fc116de4c4
Soyeb <tssoyeb@gmail.com>
2022-04-23 21:16:34 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app =express()
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors())
 app.use(express.json()) //without this we cant parse data we get from req.body
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y63bj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("Genius car DB connected");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.get('/',(req,res)=>{
